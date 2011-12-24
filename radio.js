1d3c7dfe2617145c039736aec68a36d6bfb2ddb8
hellocreation <stmhawaii@gmail.com>
2011-12-23 22:47:30 -1000

    Initial Commit
    First Commit for Radio.js

---
+++ radio.js
@@ -0,0 +1,113 @@
+/** 
+ * Author: Scott Murphy twitter: @hellocreation, github: uxder
+ * radio.js - The Chainable, Dependency Free Publish/Subscribe for Javascript
+ */
+
+(function(global) {
+	"use strict";
+	
+	radio._ = {
+		version: '0.0.1',
+		channelName: "",
+		channels: [],
+		/**
+		 * Publish
+		 */
+		broadcast: function() {
+			var		i,
+				    cn = this.channelName,
+					c = this.channels;
+			for (i in c[cn]) {
+				if(typeof(c[cn][i]) == "function") {
+					 c[cn][i].apply(c[cn][i], [arguments].splice(i,1));
+				} else if(c[cn][i][1]) {
+					c[cn][i][0].apply(c[cn][i][1], [arguments].splice(i,1));
+				} else {
+					c[cn][i][0].apply(c[cn][i][0], [arguments].splice(i,1));
+				}
+			}
+			return this;
+		},
+		
+		/**
+		 * Set the current channel name
+		 */
+		channel: function(name) {
+			this.channelName = name;
+			return this;
+		},
+		
+		/**
+		 * Add Listener to channel
+		 * @param[callback|array] callback list of callbacks separated by commas
+		 * @return [this] this returns self for chaining
+		 * @example
+		 *		//basic usage		
+		 *		radio('channel1').add(callback); //will run callback on 
+		 *		radio('channel1').add(callback, callback2, callback3 ...); //add an endless amount of callbacks
+		 * 	    
+		 *		//adding callbacks with context
+		 *  	radio('channel1').add([callback, context],[callback1, context], callback3);
+ 		 */
+		add: function() {
+				var a = arguments,
+					s = this.channels,
+					i, 
+					sn = this.channelName,
+					l= a.length;
+				//if new channel register it
+				if (!s[sn]) s[sn] = [];
+				//run through the arguments 
+				for(i=0; i<l;i++) {
+					//add listener
+					s[sn].push(a[i]);
+				}
+				return this;
+		},
+		
+		/**
+		 * Remove Listeners from Stations
+		 */
+		remove: function() {
+			var a = arguments,
+				i, 
+				l= a.length;
+			//run through the arguments 
+			for(i=0; i<l;i++) {
+				this._removeOne(a[i]);
+			}
+			return this;
+		},
+	
+		/**
+		 * Remove one listener from channel
+		 */
+		_removeOne: function(func) {
+			var s = this.channels,
+				sn = this.channelName,
+				i, 
+				l= s[sn].length;
+			
+			for(i=0; i<l;i++) {
+				if(s[sn][i] === func) s[sn].splice(i,1);
+			}
+		},
+		/**
+		 * return Array of all channels and listeners
+		 */
+		all: function(){
+			return this.channels;
+		}
+	};
+	
+	/**
+	 * Wrapper for radio
+	 */
+	function radio(channelName) {
+		radio._.channel(channelName);
+		return radio._;
+	}
+	
+	//add radio to window object
+	(global.radio) ? global.radio : global.radio = radio;
+})(window);
---
+++ tests/core.js
@@ -0,0 +1,104 @@
+/*
+ * 	Core Tests
+ */
+describe("Radio Core Test", function() {
+  beforeEach(function() {
+			f = function(){};
+			f2 = function() {};
+			f3 = function() {};
+			test = {
+				self : function() {
+					return this;
+				}
+			}
+  });
+ 	 describe("When radio is loaded", function() {
+		    it("should be global and accessible", function() {
+			  	expect(radio).toBeDefined();
+		    });
+	
+			it("should return a version number", function() {
+				 expect(radio._.version).toBeDefined();
+			});
+	  });
+	
+	 describe("radio() method", function() {
+		    it("should set the channel name", function() {
+				radio('channel1');
+			  	expect(radio._.channelName).toBe('channel1');
+		    });
+			it("requires a string", function() {
+				radio('channel1');
+			  	expect(radio._.channelName).toBe('channel1');
+		    });
+	  });
+
+	 describe("radio.channel method", function() {
+		 	it("should set the channel name", function() {
+				radio('test').channel('channel1');
+			  	expect(radio._.channelName).toBe('channel1');
+		    });
+		    it("should make multiple channels chainable", function() {
+				radio('channel1').add(f,f).channel('channel2').add(f);
+			  	expect(radio._.channelName).toBe('channel2');
+				expect(radio._.channels.channel1.length).toBe(2);
+				expect(radio._.channels.channel2.length).toBe(1);
+		    });
+	  });
+	
+	
+ 	describe("radio.add method", function() {
+					it("should register a single listener to a channel", function() {
+						 radio('channel1').add(f);
+						 expect(radio._.channels.channel1.length).toBe(1);
+					});
+					it("should add multiple listeners to a channel", function() {
+						 radio('channel1').add(f,f,f);
+						 expect(radio._.channels.channel1.length).toBe(3);
+					});
+					it("it should suppport context", function() {
+							 radio('channel1').add(f,f,f);
+							 expect(radio._.channels.channel1.length).toBe(3);
+					});
+					it("should be chainable", function() {
+						 radio('channel1').add(f).add(f2);
+						 expect(radio._.channels.channel1.length).toBe(2);
+					});
+	  });
+
+  	describe("radio.remove method", function() {
+					it("should remove a listener from a channel", function() {
+						 radio('channel1').add(f);
+						 expect(radio._.channels.channel1.length).toBe(1);
+						 radio('channel1').remove(f);
+						 expect(radio._.channels.channel1.length).toBe(0);
+					});
+					it("should remove multiple listeners from a channel", function() {
+						 radio('channel1').add(f,f2,f3);
+						 expect(radio._.channels.channel1.length).toBe(3);
+						 radio('channel1').remove(f);
+						 expect(radio._.channels.channel1.length).toBe(2);
+					});
+					it("should be chainable", function() {
+							 radio('channel1').add(f, f2, f3).remove(f2);
+							 expect(radio._.channels.channel1.length).toBe(2);
+					});
+	  });
+
+  	describe("radio.broadcast method", function() {
+					it("should call each listener only once", function() {
+					
+					});
+	  });
+	
+	afterEach(function() {
+		//clear out the channels.
+		radio._.channels = [];
+	});
+});
+
+
+
+	
+ 
+
