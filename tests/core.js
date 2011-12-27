20844fd335cd5af14796b3666921cf50737ba1e8
hellocreation <stmhawaii@gmail.com>
2011-12-26 20:33:09 -1000

    add a few more tests.

--- tests/core.js
+++ tests/core.js
@@ -1,4 +1,16 @@
 /*
+ * 	Test Object
+ */
+var	test = {
+		selfTest : function() {
+			return this;
+		},
+		add: function() {
+			1+1;
+		}
+	}
+
+/*
  * 	Core Tests
  */
 describe("Radio Core Test", function() {
@@ -6,11 +18,6 @@ describe("Radio Core Test", function() {
 			f = function(){};
 			f2 = function() {};
 			f3 = function() {};
-			test = {
-				self : function() {
-					return this;
-				}
-			}
   });
  	 describe("When radio is loaded", function() {
 		    it("should be global and accessible", function() {
@@ -45,7 +52,9 @@ describe("Radio Core Test", function() {
 				expect(radio._.channels.channel2.length).toBe(1);
 		    });
 			it("should create a new channel if it doesn't exists", function() {
-
+				expect(radio._.channels['test']).not.toBeDefined();
+				radio('test');
+				expect(radio._.channels['test']).toBeDefined();
 			});
 
 	  });
@@ -53,16 +62,26 @@ describe("Radio Core Test", function() {
 	
  	describe("radio.add method", function() {
 					it("should register a single listener to a channel", function() {
-						 radio('channel1').add(f);
+						 var test = radio('channel1').add(f);
 						 expect(radio._.channels.channel1.length).toBe(1);
 					});
 					it("should add multiple listeners to a channel", function() {
 						 radio('channel1').add(f,f,f);
 						 expect(radio._.channels.channel1.length).toBe(3);
 					});
-					it("it should suppport context", function() {
-							 radio('channel1').add(f,f,f);
-							 expect(radio._.channels.channel1.length).toBe(3);
+					it("it should suppport adding listerners with setting the context of 'this'", function() {
+							 radio('channel1').add([test.selfTest, test]);
+							 expect(radio._.channels.channel1.length).toBe(1);
+					});
+					it("it should a combination of adding functions and anonymous functions", function() {
+								 radio('channel1').add(f,[test.selfTest, test], f2);
+								 expect(radio._.channels.channel1.length).toBe(3);
+					});
+					it("it should not allow a non-function or array to be added", function() {
+									 radio('channel1').add("string", f);
+									 expect(radio._.channels.channel1.length).toBe(1);
+									 radio('channel1').add(2, f);
+									 expect(radio._.channels.channel1.length).toBe(2);
 					});
 					it("should be chainable", function() {
 						 radio('channel1').add(f).add(f2);
@@ -83,6 +102,12 @@ describe("Radio Core Test", function() {
 						 radio('channel1').remove(f);
 						 expect(radio._.channels.channel1.length).toBe(2);
 					});
+					it("should remove listeners add with context from channel", function() {
+						 radio('channel1').add([test.selfTest, test], f);
+						 expect(radio._.channels.channel1.length).toBe(2);
+						 radio('channel1').remove(test.selfTest);
+						 expect(radio._.channels.channel1.length).toBe(1);
+					});
 					it("should be chainable", function() {
 							 radio('channel1').add(f, f2, f3).remove(f2);
 							 expect(radio._.channels.channel1.length).toBe(2);
@@ -91,7 +116,13 @@ describe("Radio Core Test", function() {
 
   	describe("radio.broadcast method", function() {
 					it("should call each listener only once", function() {
-					
+						spyOn(test, 'add');
+						spyOn(test, 'selfTest');
+						radio('channel1').add([test.selfTest, test], [test.add, test]);
+						expect(radio._.channels.channel1.length).toBe(2);
+						radio('channel1').broadcast('test');
+						expect(test.selfTest).toHaveBeenCalled();
+						expect(test.add).toHaveBeenCalled();	
 					});
 					it("should not fail even if the channel has never been created", function() {
 					
@@ -108,6 +139,6 @@ describe("Radio Core Test", function() {
 
 
 
-	
+
  
 
